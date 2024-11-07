fc6f81dbb28d8b698f6535453df52be53eb61575
Franciscocosta <160623601+franciscorcosta@users.noreply.github.com>
2024-11-07 10:52:51 +0000

    meti o mousein e o mouseout na mensagem, fiz a os botões para mudar de cor no texto "Pinta-me!" e as funções que muda a cor da caixa do texto como cores definidas

--- lab7/main.js
+++ lab7/main.js
@@ -1,15 +1,65 @@
-// butões cores 
+// mousein e mouseout da mensagem
+const mensagem = document.getElementById('mensagem');
+
+mensagem.addEventListener('mouseover', function () {
+    this.textContent = 'Obrigado por passares!';
+});
+
+mensagem.addEventListener('mouseout', function () {
+    this.textContent = 'Passa por aqui!';
+})
+
+
+// botões cores 
+const botaoVermelho = document.getElementById('vermelho');
+const botaoVerde = document.getElementById('verde');
+const botaoAzul = document.getElementById('azul');
+const texto = document.getElementById('texto');
+
+// Função para mudar a cor do texto
+function mudarCorTexto(cor) {
+    texto.style.color = cor;
+}
+
+// Eventos de clique nos botões
+
+// Vermelho
+botaoVermelho.addEventListener('click', function () {
+    mudarCorTexto('red');
+});
+
+// Verde
+botaoVerde.addEventListener('click', function () {
+    mudarCorTexto('green');
+});
+
+// Azul
+botaoAzul.addEventListener('click', function () {
+    mudarCorTexto('blue');
+});
+
+
+// Ao escrever muda a cor da caixa de texto com cores definidas 
+function corAleatoria() {
+    const colors = ['blue', 'red', 'yellow', 'grey'];
+    return colors[Math.floor(Math.random() * colors.length)];
+}
+
+function mudacorCaixaTexto() {
+    var inputBox = document.getElementById('textInput');
+    inputBox.style.backgroundColor = corAleatoria();
+}
 
 
 
-// vermelho
-
-// verde
-
-// azul
+// Ao escrever uma cor em inglês seguindo de clinar no submeter muda a cor de fundo da página
 
 
-//botão submeter
+
+
+
+
+
 
 
 
@@ -21,5 +71,5 @@ function contar() {
     document.querySelector('#contador').innerHTML = count;
 }
 
-// Corrigido: passa a função contar no onclick
+// Passa a função contar no onclick
 document.querySelector('#conta').onclick = contar;
