561181088bca49060e3828d8b4823d70e8c724ed
Franciscocosta <160623601+franciscorcosta@users.noreply.github.com>
2024-11-07 09:58:12 +0000

    contador com botão

--- lab7/main.js
+++ lab7/main.js
@@ -1,3 +1,25 @@
-function mudatexto(){
+// butões cores 
 
+
+
+// vermelho
+
+// verde
+
+// azul
+
+
+//botão submeter
+
+
+
+// contador com botão
+let count = 0;
+
+function contar() {
+    count++;
+    document.querySelector('#contador').innerHTML = count;
 }
+
+// Corrigido: passa a função contar no onclick
+document.querySelector('#conta').onclick = contar;
