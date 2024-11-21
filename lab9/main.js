0a3ae065b5b8e514b9a4fa85468d0ac928c326a9
Franciscocosta <160623601+franciscorcosta@users.noreply.github.com>
2024-11-21 16:50:36 +0000

    criei imagens

--- lab9/main.js
+++ lab9/main.js
@@ -4,14 +4,20 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
+
+
+
+
 function carregarProdutos(produtos) {
     produtos.forEach(produto => {
 
         const article = criarProduto(produto);
         document.querySelector('#listaprodutos').append(article);
-
         
 
+       const imagem = criarProduto(produto)
+       document.querySelector('#listaprodutos').append(imagem)
+ 
 
 
     });
@@ -28,6 +34,22 @@ function criarProduto(produto) {
     const titulo = document.createElement('h2')
     titulo.textContent = produto.title
     article.append(titulo)
+
+    //  Cria a imagem do produto mas está a aparecer em duplicado (Resover depois)
+    const imagem = document.createElement('img')
+    imagem.src = produto.image
+    imagem.alt = produto.title; //Usando o título como texto alternativo para a imagem
+    article.append(imagem)
+
+
+    // Cria a descrição do produto
+    
+
+
+
+
+
+
 
     return article;
 }
